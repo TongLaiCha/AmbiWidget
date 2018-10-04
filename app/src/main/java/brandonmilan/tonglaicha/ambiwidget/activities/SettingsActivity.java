4df8404a145da05a2a0e82dd95d5319d079d379d
BrandonYuen <brandon97@hotmail.com>
2018-10-04 17:43:55 +0800

    Button Feedback & Settings Icons

--- app/src/main/java/brandonmilan/tonglaicha/ambiwidget/activities/SettingsActivity.java
+++ app/src/main/java/brandonmilan/tonglaicha/ambiwidget/activities/SettingsActivity.java
@@ -28,6 +28,10 @@ public class SettingsActivity extends AppCompatActivity {
 		doneBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
+				Intent startMain = new Intent(Intent.ACTION_MAIN);
+				startMain.addCategory(Intent.CATEGORY_HOME);
+				startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(startMain);
 				finish();
 			}
 		});

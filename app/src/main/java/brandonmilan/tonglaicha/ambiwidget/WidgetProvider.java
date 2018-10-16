29d89ca64036a3eb6dcaba528eae2c86873196c7
Milan <msosef@yahoo.com>
2018-10-16 11:08:40 +0800

    Refactor code for update progressbar.

--- app/src/main/java/brandonmilan/tonglaicha/ambiwidget/WidgetProvider.java
+++ app/src/main/java/brandonmilan/tonglaicha/ambiwidget/WidgetProvider.java
@@ -6,9 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.view.View;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 import brandonmilan.tonglaicha.ambiwidget.API.TokenManager;
 import brandonmilan.tonglaicha.ambiwidget.activities.AuthActivity;
@@ -23,137 +21,134 @@ import brandonmilan.tonglaicha.ambiwidget.utils.WidgetUtils;
  * @author Milan Sosef
  */
 public class WidgetProvider extends AppWidgetProvider {
-    private static final String TAG = "WidgetProvider";
-    private static final String TooColdTag = "too_cold";
-    private static final String LittleColdTag = "bit_cold";
-    private static final String ComfyTag = "comfortable";
-    private static final String LittleWarmTag = "bit_warm";
-    private static final String TooWarmTag = "too_warm";
-    private static final String ActionFeedback = WidgetService.ACTION_GIVE_FEEDBACK;
-    private static final String ActionUpdate = WidgetService.ACTION_UPDATE_WIDGET;
-    public static final String UpdateByUserTag = "update_by_user";
-    private static final String ActionSwitchOnOff = WidgetService.ACTION_SWITCH_ON_OFF;
-    private static final Integer JOB_ID = 10;
+	private static final String TAG = "WidgetProvider";
+	private static final String TooColdTag = "too_cold";
+	private static final String LittleColdTag = "bit_cold";
+	private static final String ComfyTag = "comfortable";
+	private static final String LittleWarmTag = "bit_warm";
+	private static final String TooWarmTag = "too_warm";
+	private static final String ActionFeedback = WidgetService.ACTION_GIVE_FEEDBACK;
+	private static final String ActionUpdate = WidgetService.ACTION_UPDATE_WIDGET;
+	public static final String UpdateByUserTag = "update_by_user";
+	private static final String ActionSwitchOnOff = WidgetService.ACTION_SWITCH_ON_OFF;
+	private static final Integer JOB_ID = 10;
 
-    /**
-     * Instruct the appWidgetManager to load the widgets view and its components.
-     */
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Boolean updateFromUser) {
-        Log.d(TAG, "UPDATING WIDGET WITH ID = "+appWidgetId);
+	/**
+	 * Instruct the appWidgetManager to load the widgets view and its components.
+	 */
+	static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Boolean updateFromUser) {
+		Log.d(TAG, "UPDATING WIDGET WITH ID = "+appWidgetId);
 
-        //Check if the user has authorized the widget to access his Ambi account.
-        if(TokenManager.getRefreshToken(context).value() == null){
-            Log.d(TAG, "REFRESH TOKEN NOT SET: Constructing widget widget_auth_overlay");
-            // Construct the RemoteViews object
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_auth_overlay);
+		//Check if the user has authorized the widget to access his Ambi account.
+		if(TokenManager.getRefreshToken(context).value() == null){
+			// Construct the RemoteViews object
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_auth_overlay);
 
-            Intent authIntent = new Intent(context, AuthActivity.class);
-            authIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            PendingIntent configPendingIntent = PendingIntent.getActivity(context, appWidgetId, authIntent, 0);
-            views.setOnClickPendingIntent(R.id.button_authorize, configPendingIntent);
+			Intent authIntent = new Intent(context, AuthActivity.class);
+			authIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+			PendingIntent configPendingIntent = PendingIntent.getActivity(context, appWidgetId, authIntent, 0);
+			views.setOnClickPendingIntent(R.id.button_authorize, configPendingIntent);
 
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        } else {
-            Log.d(TAG, "REFRESH TOKEN IS SET: Constructing widget full_widget");
-            // Construct the RemoteViews object
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.full_widget);
+			// Instruct the widget manager to update the widget
+			appWidgetManager.updateAppWidget(appWidgetId, views);
+		} else {
+			// Construct the RemoteViews object
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.full_widget);
 
-            //Display loading animation when the user clicks the refresh button.
-            if(updateFromUser){
-                views.setViewVisibility(R.id.button_refresh, View.INVISIBLE);
-                views.setViewVisibility(R.id.progressBar, View.VISIBLE);
-            }
+			//Display loading animation when the user clicks the refresh button.
+			if(updateFromUser){
+				WidgetUtils.updateRefreshAnimation(true, views);
+			}
 
-            setButtonClickHandlers(context, appWidgetId, views);
+			setButtonClickHandlers(context, appWidgetId, views);
 
-            //Update the temperature, humidity, room name and location name.
-            WidgetContentManager.updateAllViews(context, views, appWidgetId);
+			//Update the temperature, humidity, room name and location name.
+			WidgetContentManager.updateAllViews(context, views, appWidgetId);
 
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+			// Instruct the widget manager to update the widget
+			appWidgetManager.updateAppWidget(appWidgetId, views);
 
-            Log.d(TAG, "updateAppWidget: Success!");
-        }
-    }
+			Log.d(TAG, "updateAppWidget: Success!");
+		}
+	}
 
-    /**
-     * Set all click handlers for the widgets buttons.
-     */
-    private static void setButtonClickHandlers(Context context, int appWidgetId, RemoteViews views) {
-        //Set onClickPendingIntents for all the feedback buttons.
-        views.setOnClickPendingIntent(R.id.button_too_cold, WidgetUtils.getPendingIntent(context, ActionFeedback, TooColdTag));
-        views.setOnClickPendingIntent(R.id.button_little_cold, WidgetUtils.getPendingIntent(context, ActionFeedback, LittleColdTag));
-        views.setOnClickPendingIntent(R.id.button_comfy, WidgetUtils.getPendingIntent(context, ActionFeedback, ComfyTag));
-        views.setOnClickPendingIntent(R.id.button_little_warm, WidgetUtils.getPendingIntent(context, ActionFeedback, LittleWarmTag));
-        views.setOnClickPendingIntent(R.id.button_too_warm, WidgetUtils.getPendingIntent(context, ActionFeedback, TooWarmTag));
+	/**
+	 * Set all click handlers for the widgets buttons.
+	 */
+	private static void setButtonClickHandlers(Context context, int appWidgetId, RemoteViews views) {
+		//Set onClickPendingIntents for all the feedback buttons.
+		views.setOnClickPendingIntent(R.id.button_too_cold, WidgetUtils.getPendingIntent(context, ActionFeedback, TooColdTag));
+		views.setOnClickPendingIntent(R.id.button_little_cold, WidgetUtils.getPendingIntent(context, ActionFeedback, LittleColdTag));
+		views.setOnClickPendingIntent(R.id.button_comfy, WidgetUtils.getPendingIntent(context, ActionFeedback, ComfyTag));
+		views.setOnClickPendingIntent(R.id.button_little_warm, WidgetUtils.getPendingIntent(context, ActionFeedback, LittleWarmTag));
+		views.setOnClickPendingIntent(R.id.button_too_warm, WidgetUtils.getPendingIntent(context, ActionFeedback, TooWarmTag));
 
-        //Set onClickPendingIntent for on/off button.
-        views.setOnClickPendingIntent(R.id.button_on_off, WidgetUtils.getPendingIntent(context, ActionSwitchOnOff, null));
+		//Set onClickPendingIntent for on/off button.
+		views.setOnClickPendingIntent(R.id.button_on_off, WidgetUtils.getPendingIntent(context, ActionSwitchOnOff, null));
 
-        //Set onClickPendingIntent for the settings button.
-        Intent configIntent = new Intent(context, SettingsActivity.class);
-        //WARNING: Include the widget ID with the pendingIntent, or the configuration activity will not be opened.
-        configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        PendingIntent configPendingIntent = PendingIntent.getActivity(context, appWidgetId, configIntent, 0);
-        views.setOnClickPendingIntent(R.id.button_settings, configPendingIntent);
+		//Set onClickPendingIntent for the settings button.
+		Intent configIntent = new Intent(context, SettingsActivity.class);
+		//WARNING: Include the widget ID with the pendingIntent, or the configuration activity will not be opened.
+		configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+		PendingIntent configPendingIntent = PendingIntent.getActivity(context, appWidgetId, configIntent, 0);
+		views.setOnClickPendingIntent(R.id.button_settings, configPendingIntent);
 
-        //Set onClickPendingIntent for the refresh button.
-        views.setOnClickPendingIntent(R.id.button_refresh,
-                WidgetUtils.getPendingIntent(context, ActionUpdate, UpdateByUserTag));
-    }
+		//Set onClickPendingIntent for the refresh button.
+		views.setOnClickPendingIntent(R.id.button_refresh,
+				WidgetUtils.getPendingIntent(context, ActionUpdate, UpdateByUserTag));
+	}
 
-    //TODO: Make onUpdate only execute after the configuration is done.
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.d(TAG, "onUpdate: Executed...");
+	//TODO: Make onUpdate only execute after the configuration is done.
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		Log.d(TAG, "onUpdate: Executed...");
 
-        WidgetUtils.remoteUpdateWidget(context);
+		WidgetUtils.remoteUpdateWidget(context);
 
-    }
+	}
 
-    /**
-     * Update all widgets currently active on the screen.
-     */
-    public static void updateAllWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds, Boolean updateFromUser) {
-        Log.d(TAG, "updateAllWidgets: Executed..");
-        Log.d(TAG, "ALL WIDGET ID'S= " + appWidgetIds);
+	/**
+	 * Update all widgets currently active on the screen.
+	 */
+	public static void updateAllWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds, Boolean updateFromUser) {
+		Log.d(TAG, "updateAllWidgets: Executed..");
+		Log.d(TAG, "ALL WIDGET ID'S= " + appWidgetIds);
 
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId, updateFromUser);
-        }
-    }
+		// There may be multiple widgets active, so update all of them
+		for (int appWidgetId : appWidgetIds) {
+			updateAppWidget(context, appWidgetManager, appWidgetId, updateFromUser);
+		}
+	}
 
-    /**
-     * Recieves and handles broadcast intents sent to the {@link WidgetProvider WidgetProvider}.
-     * This method will enqueue new work to be dispatched to and handled by the {@link WidgetService }.
-     */
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        Log.d(TAG, "onReceive()" + intent.getAction());
+	/**
+	 * Recieves and handles broadcast intents sent to the {@link WidgetProvider WidgetProvider}.
+	 * This method will enqueue new work to be dispatched to and handled by the {@link WidgetService }.
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		Log.d(TAG, "onReceive()" + intent.getAction());
 
-        WidgetService.enqueueWork(context, WidgetService.class, JOB_ID, intent);
-    }
+		WidgetService.enqueueWork(context, WidgetService.class, JOB_ID, intent);
+	}
 
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        // When the user deletes the widget, delete the preference associated with it.
-        for (int appWidgetId : appWidgetIds) {
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		// When the user deletes the widget, delete the preference associated with it.
+		for (int appWidgetId : appWidgetIds) {
 //            WidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
-        }
-    }
+		}
+	}
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
+	@Override
+	public void onEnabled(Context context) {
+		// Enter relevant functionality for when the first widget is created
+	}
 
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
+	@Override
+	public void onDisabled(Context context) {
+		// Enter relevant functionality for when the last widget is disabled
+	}
 
 }
 
